e2ccc7dd1eaa1ae8b3367577276ebe8b225e5e27
mhkmcp <mhkmcp@yahoo.com>
2021-02-16 00:55:41 +0600

    04: Write File Async & Sync with fs module

--- fsModule/index.js
+++ fsModule/index.js
@@ -3,7 +3,37 @@ var http = require('http')
 
 var server = http.createServer(function (request, response) {
     if (request.url == '/') {
-        // async
+
+        // async write 
+        // fs.writeFile('demo.txt', 'Hello Humayun', function (error) {
+        //     if (error) {
+        //         response.writeHead(200, { 'Content-Type': 'text/html' })
+        //         response.write("File Write Failed!")
+        //         response.end()
+
+        //     } else {
+        //         response.writeHead(200, { 'Content-Type': 'text/html' })
+        //         response.write("File Write Succeded!")
+        //         response.end()
+        //     }
+        // })
+
+        // sync write 
+        let error = fs.writeFileSync('demo.txt', 'Welcome Humayun');
+
+        if (error) {
+            response.writeHead(200, { 'Content-Type': 'text/html' })
+            response.write("File Write Failed!")
+            response.end()
+
+        } else {
+            response.writeHead(200, { 'Content-Type': 'text/html' })
+            response.write("File Write Succeded!")
+            response.end()
+        }
+
+
+        // async read
         // fs.readFile('home.html', function (error, data) {
         //     response.writeHead(200, { 'Content-Type': 'text/html' })
         //     response.write(data)
@@ -11,11 +41,11 @@ var server = http.createServer(function (request, response) {
 
         // })
 
-        // sync
-        let myData = fs.readFileSync('home.html');
-        response.writeHead(200, { 'Content-Type': 'text/html' })
-        response.write(myData)
-        response.end()
+        // sync read
+        // let myData = fs.readFileSync('home.html');
+        // response.writeHead(200, { 'Content-Type': 'text/html' })
+        // response.write(myData)
+        // response.end()
     }
 })
 server.listen(5050)
