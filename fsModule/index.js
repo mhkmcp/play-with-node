a8112cffb03f911eb6f2385585b469e42eb157f8
mhkmcp <mhkmcp@yahoo.com>
2021-02-16 01:03:06 +0600

    05: Rename File Async & Sync with fs module

--- fsModule/index.js
+++ fsModule/index.js
@@ -3,6 +3,33 @@ var http = require('http')
 
 var server = http.createServer(function (request, response) {
     if (request.url == '/') {
+        // async rename 
+        // fs.rename('demo.txt', 'demoNew.txt', function (error) {
+        //     if (error) {
+        //         response.writeHead(200, { 'Content-Type': 'text/html' })
+        //         response.write("File Rename Failed!")
+        //         response.end()
+
+        //     } else {
+        //         response.writeHead(200, { 'Content-Type': 'text/html' })
+        //         response.write("File Rename Succeded!")
+        //         response.end()
+        //     }
+        // })
+
+        // sync rename 
+        let error = fs.renameSync('demoNew.txt', 'demo.txt');
+
+        if (error) {
+            response.writeHead(200, { 'Content-Type': 'text/html' })
+            response.write("File Rename Failed!")
+            response.end()
+
+        } else {
+            response.writeHead(200, { 'Content-Type': 'text/html' })
+            response.write("File Rename Succeded!")
+            response.end()
+        }
 
         // async write 
         // fs.writeFile('demo.txt', 'Hello Humayun', function (error) {
@@ -19,18 +46,18 @@ var server = http.createServer(function (request, response) {
         // })
 
         // sync write 
-        let error = fs.writeFileSync('demo.txt', 'Welcome Humayun');
+        // let error = fs.writeFileSync('demo.txt', 'Welcome Humayun');
 
-        if (error) {
-            response.writeHead(200, { 'Content-Type': 'text/html' })
-            response.write("File Write Failed!")
-            response.end()
+        // if (error) {
+        //     response.writeHead(200, { 'Content-Type': 'text/html' })
+        //     response.write("File Write Failed!")
+        //     response.end()
 
-        } else {
-            response.writeHead(200, { 'Content-Type': 'text/html' })
-            response.write("File Write Succeded!")
-            response.end()
-        }
+        // } else {
+        //     response.writeHead(200, { 'Content-Type': 'text/html' })
+        //     response.write("File Write Succeded!")
+        //     response.end()
+        // }
 
 
         // async read
