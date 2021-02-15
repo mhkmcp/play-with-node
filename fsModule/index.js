2a46cb8808cb02140cf6713f08a6ab0fd3c10be9
mhkmcp <mhkmcp@yahoo.com>
2021-02-16 00:42:53 +0600

    03: Read HTML File with Sync fs Module

--- fsModule/index.js
+++ fsModule/index.js
@@ -4,12 +4,18 @@ var http = require('http')
 var server = http.createServer(function (request, response) {
     if (request.url == '/') {
         // async
-        fs.readFile('home.html', function (error, data) {
-            response.writeHead(200, { 'Content-Type': 'text/html' })
-            response.write(data)
-            response.end()
+        // fs.readFile('home.html', function (error, data) {
+        //     response.writeHead(200, { 'Content-Type': 'text/html' })
+        //     response.write(data)
+        //     response.end()
 
-        })
+        // })
+
+        // sync
+        let myData = fs.readFileSync('home.html');
+        response.writeHead(200, { 'Content-Type': 'text/html' })
+        response.write(myData)
+        response.end()
     }
 })
 server.listen(5050)
